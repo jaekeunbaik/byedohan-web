5ae44825e0a8c422a6cd1c9c816ff91052383f8f
edohan <jaekeunbaik@gmail.com>
2025-04-15 22:26:49 +0900

    이름 수정

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-100 dark:bg-gray-800 py-8 text-center text-gray-600 dark:text-gray-400">
+      <p>© 2025 홍길동. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
